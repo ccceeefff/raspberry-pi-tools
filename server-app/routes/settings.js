18cd7c823c4fd50453a52d8bd603e0daec5b9c2a
Cef Ramirez <cef.ramirez@gmail.com>
2015-11-09 16:34:35 -0800

    adding validations

--- server-app/routes/settings.js
+++ server-app/routes/settings.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var models  = require('../models');
 
+var validator = require('validator');
+
 /*
 router.get('/', function(req, res, next) {
   var record = models.Record.build({
@@ -36,6 +38,33 @@ function getSettings(next){
 	});
 }
 
+function verify_data(data, next){
+	var errorStr = null;
+	if(validator.isNull(data)){
+		errorStr = "Submission contains no data";
+	} else if(validator.isNull(data.name)){
+		errorStr = "Invalid name";
+	} else if(validator.isNull(data.cloud_server_address)){
+		errorStr = "Invalid server address";
+	} else if(!validator.isInt(data.submission_interval)){
+		errorStr = "Invalid submission interval";
+	} else if(!validator.isInt(data.poll_interval)){
+		errorStr = "Invalid poll interval";
+	} else if(validator.isNull(data.location)){
+		errorStr = "Invalid location";
+	} else if(!validator.isNumeric(data.location.lat)){
+		errorStr = "Invalid location latitude";
+	} else if(!validator.isNumeric(data.location.long)){
+		errorStr = "Invalid location longitude";
+	}
+	console.log("error: " + errorStr);
+	if(errorStr !== null){
+		next(new Error(errorStr));
+	} else {
+		next();
+	}
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 	getSettings(function(settings){
@@ -44,14 +73,21 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next){
-	settings.name = req.body.name;
-	settings.cloud_server_address = req.body.cloud_server_address;
-	settings.poll_interval = req.body.poll_interval;
-	settings.submission_interval = req.body.submission_interval;
-	settings.location.lat = req.body.location.lat;
-	settings.location.long = req.body.location.long;
-	settings.updated_at = Math.floor(new Date() / 1000);
-	res.json(settings);
+	// verify values
+	verify_data(req.body, function(error){
+		if(error){
+			res.status(400).json({error: error.message});
+		} else {
+			settings.name = req.body.name;
+			settings.cloud_server_address = req.body.cloud_server_address;
+			settings.poll_interval = req.body.poll_interval;
+			settings.submission_interval = req.body.submission_interval;
+			settings.location.lat = req.body.location.lat;
+			settings.location.long = req.body.location.long;
+			settings.updated_at = Math.floor(new Date() / 1000);
+			res.json(settings);
+		}
+	});
 });
 
 module.exports = router;
