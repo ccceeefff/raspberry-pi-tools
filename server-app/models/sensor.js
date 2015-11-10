b74059eab6474c183266a5835422a799305639b0
Cef Ramirez <cef.ramirez@gmail.com>
2015-11-10 13:52:26 -0800

    add additional fields to sensors

--- server-app/models/sensor.js
+++ server-app/models/sensor.js
@@ -3,8 +3,10 @@
 module.exports = function(sequelize, DataTypes) {
   var Sensor = sequelize.define("Sensor", {
     nodeId: DataTypes.STRING,
+    pipeAddress: DataTypes.STRING,
     pollInterval: DataTypes.INTEGER,
-    lastValue: DataTypes.INTEGER
+    lastValue: DataTypes.INTEGER,
+    lastTransmission: DataTypes.INTEGER
   });
 
   return Sensor;
