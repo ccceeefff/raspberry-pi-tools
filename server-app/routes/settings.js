1e5b988a4e1fd144c272817d276b83e5c5cc7048
Cef Ramirez <cef.ramirez@gmail.com>
2015-11-09 17:03:23 -0800

    cleaning up settings api

--- server-app/routes/settings.js
+++ server-app/routes/settings.js
@@ -4,27 +4,6 @@ var model  = require('../models');
 
 var validator = require('validator');
 
-/*
-router.get('/', function(req, res, next) {
-  var record = models.Record.build({
-    nodeId: 'abcde',
-    value: 12345,
-    timestamp: Date.now() / 1000, // Timetamp in seconds
-    submitted: 0
-  });
-
-  record.save().then(function() {
-    models.Record.findAll()
-      .then(function(data) {
-        console.log(data);
-        res.send('responds with settings info');
-      });
-  });
-*/
-/*
- * my temporary sqlite storage
- */
-
 function getSettings(next){
 	model.Setting.findOrCreate({where: {id: 1}, defaults: {
 		name: "",
@@ -40,23 +19,23 @@ function getSettings(next){
 
 function verify_data(data, next){
 	var errorStr = null;
-	// if(validator.isNull(data)){
-	// 	errorStr = "Submission contains no data";
-	// } else if(validator.isNull(data.name)){
-	// 	errorStr = "Invalid name";
-	// } else if(validator.isNull(data.cloud_server_address)){
-	// 	errorStr = "Invalid server address";
-	// } else if(!validator.isInt(data.submission_interval)){
-	// 	errorStr = "Invalid submission interval";
-	// } else if(!validator.isInt(data.poll_interval)){
-	// 	errorStr = "Invalid poll interval";
-	// } else if(validator.isNull(data.location)){
-	// 	errorStr = "Invalid location";
-	// } else if(!validator.isNumeric(data.location.lat)){
-	// 	errorStr = "Invalid location latitude";
-	// } else if(!validator.isNumeric(data.location.long)){
-	// 	errorStr = "Invalid location longitude";
-	// }
+
+	if(validator.isNull(data)){
+		errorStr = "Submission contains no data";
+	} else if(validator.isNull(data.name)){
+		errorStr = "Invalid name";
+	} else if(validator.isNull(data.cloud_server_address)){
+		errorStr = "Invalid server address";
+	} else if(!validator.isInt(data.submission_interval)){
+		errorStr = "Invalid submission interval";
+	} else if(!validator.isInt(data.poll_interval)){
+		errorStr = "Invalid poll interval";
+	} else if(isNaN(validator.toFloat(data.locLat))){
+		errorStr = "Invalid location latitude";
+	} else if(isNaN(validator.isNumeric(data.locLong))){
+		errorStr = "Invalid location longitude";
+	}
+
 	console.log("error: " + errorStr);
 	if(errorStr !== null){
 		next(new Error(errorStr));
@@ -81,10 +60,10 @@ router.post('/', function(req, res, next){
 			getSettings(function(settings){
 				settings.name = req.body.name;
 				settings.cloud_server_address = req.body.cloud_server_address;
-				settings.poll_interval = req.body.poll_interval;
-				settings.submission_interval = req.body.submission_interval;
-				settings.locLat = req.body.location.lat;
-				settings.locLong = req.body.location.long;
+				settings.poll_interval = validator.toInt(req.body.poll_interval);
+				settings.submission_interval = validator.toInt(req.body.submission_interval);
+				settings.locLat = validator.toFloat(req.body.locLat);
+				settings.locLong = validator.toFloat(req.body.locLong);
 				settings.save();
 				res.json(settings);
 			});
