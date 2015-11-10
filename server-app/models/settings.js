826304f236243eb7b3db886fb452b56bbd66fa5c
Cef Ramirez <cef.ramirez@gmail.com>
2015-11-09 16:06:53 -0800

    adding basic fetch and set api

---
+++ server-app/models/settings.js
@@ -0,0 +1,12 @@
+module.exports = function(sequelize, DataTypes) {
+  var Setting = sequelize.define("Setting", {
+  	name: DataTypes.STRING,
+	cloud_server_address: DataTypes.STRING,
+	poll_interval: DataTypes.INTEGER,
+	submission_interval: DataTypes.INTEGER,
+	locLat: DataTypes.FLOAT,
+	locLong: DataTypes.FLOAT
+  });
+
+  return Setting;
+};
--- server-app/routes/settings.js
+++ server-app/routes/settings.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var models  = require('../models');
 
+/*
 router.get('/', function(req, res, next) {
   var record = models.Record.build({
     nodeId: 'abcde',
@@ -17,10 +18,40 @@ router.get('/', function(req, res, next) {
         res.send('responds with settings info');
       });
   });
+*/
+/*
+ * my temporary sqlite storage
+ */
+
+function getSettings(next){
+	model.Settings.findOrCreate({where: {id: 1}, defaults: {
+		name: "",
+		cloud_server_address: "",
+		poll_interval: 0,
+		submission_interval: 0,
+		locLat: 0.0,
+		locLong: 0.0
+	}}).spread(function(settings, created){
+		next(settings);
+	});
+}
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+	getSettings(function(settings){
+		res.json(settings);	
+	})d
 });
 
 router.post('/', function(req, res, next){
-  res.send('submits settings info');
+	settings.name = req.body.name;
+	settings.cloud_server_address = req.body.cloud_server_address;
+	settings.poll_interval = req.body.poll_interval;
+	settings.submission_interval = req.body.submission_interval;
+	settings.location.lat = req.body.location.lat;
+	settings.location.long = req.body.location.long;
+	settings.updated_at = Math.floor(new Date() / 1000);
+	res.json(settings);
 });
 
 module.exports = router;
