26c6d52b5c7d2d7857078fe8e46adc44b10ef2fd
Samudra Harapan Bekti <samudra.bekti@gmail.com>
2015-11-12 14:56:59 -0800

    FIx again

--- server-app/controllers/uploader.js
+++ server-app/controllers/uploader.js
@@ -22,7 +22,7 @@ function Uploader(){
 Uploader.prototype.run = function(){
 	var self = this;
 	getSettings(function(settings){
-		setTimeout(self.run, settings.submission_interval * 1000);
+		setTimeout(function() {self.run()}, settings.submission_interval * 1000);
 
 		Record.findAll({where : {submitted: 0}}).then(function(records){
 			self.submit(settings, records, function(error){
