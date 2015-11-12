8594e34fcfbdf03e3f688e660493bba4beb10070
Cef Ramirez <cef.ramirez@gmail.com>
2015-11-11 18:47:29 -0800

    adding gateway info

--- server-app/controllers/uploader.js
+++ server-app/controllers/uploader.js
@@ -1,5 +1,19 @@
 var http = require('http');
 var Record = require('../models').Record;
+var Setting = require('../models').Setting;
+
+function getSettings(next){
+	Setting.findOrCreate({where: {id: 1}, defaults: {
+		name: '',
+		cloud_server_address: '',
+		poll_interval: 0,
+		submission_interval: 0,
+		locLat: 0.0,
+		locLong: 0.0
+	}}).spread(function(settings, created){
+		next(settings);
+	});
+}
 
 function Uploader(host, port){
 	this.host = host;
@@ -8,19 +22,16 @@ function Uploader(host, port){
 
 Uploader.prototype.run = function(){
 	var self = this;
-	Record.findAll({where : {submitted: 0}}).then(function(records){
-		self.submit({
-			macAddr: "1234",
-			locLat: 1.23,
-			locLong: 1.23
-		}
-			, records, function(error){
-			if(error === null){
-				records.forEach(function(record){
-					record.submitted = 1;
-					record.save();
-				});
-			}
+	getSettings(function(settings){
+		Record.findAll({where : {submitted: 0}}).then(function(records){
+			self.submit(settings, records, function(error){
+				if(error === null){
+					records.forEach(function(record){
+						record.submitted = 1;
+						record.save();
+					});
+				}
+			});
 		});
 	});
 };
@@ -42,7 +53,7 @@ Uploader.prototype.submit = function(gatewayInfo, items, next){
     var entries = [];
     items.forEach(function(item){
     	var entry = {
-    		nodeId: item.nodeId,
+    		nodeId: item.address,
     		rawValue: item.value,
     		timestamp: item.createdAt
     	};
@@ -50,7 +61,7 @@ Uploader.prototype.submit = function(gatewayInfo, items, next){
     });
     
     var data = {
-    	gatewayId: gatewayInfo.macAddr,
+    	gatewayId: gatewayInfo.name,
     	locLat: gatewayInfo.locLat,
     	locLong: gatewayInfo.locLong,
     	entries: entries
